ee928f07a53a1988d205081a354e3f520d1ff293
d.lihodedov <likhodedovdmitry@gmail.com>
2016-11-10 19:31:48 +0700

    Add project files.

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+       // GET api/<controller>/5
+        public IEnumerable<string> Get(String Name, String Description)
+        {
+          string[] hello =  new string[] { Name, Description };
+            return hello;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
